48618f1b5a13bd585d69c6df54dc554aed554e92
Nicholas <nicholas.yee@outlook.com>
2018-08-05 22:17:47 +1000

    IM A GOD

--- app/src/main/java/com/mad/exercise2/ActivityTwo.java
+++ app/src/main/java/com/mad/exercise2/ActivityTwo.java
@@ -1,13 +1,55 @@
 package com.mad.exercise2;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.constraint.Constraints;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ActivityTwo extends AppCompatActivity {
+
+    public static final String NAME_EXTRA = "@string/your_name";
+    public static final String PHONE_EXTRA = "@string/your_phone";
+    public static final String EMAIL_EXTRA = "@string/your_email";
+    public static final String TYPE_EXTRA = "@string/your_type";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two);
+
+        Intent intent = getIntent();
+
+        TextView name = findViewById(R.id.second_activity_name);
+        name.setText(intent.getStringExtra(NAME_EXTRA));
+        TextView phone = findViewById(R.id.second_activity_phone);
+        phone.setText(intent.getStringExtra(PHONE_EXTRA));
+        TextView email = findViewById(R.id.second_activity_email);
+        email.setText(intent.getStringExtra(EMAIL_EXTRA));
+        TextView type = findViewById(R.id.second_activity_type);
+        type.setText(intent.getStringExtra(TYPE_EXTRA));
+
+
+
+        Button submitBtn = findViewById(R.id.second_activity_submit);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CheckBox agreeCB = findViewById(R.id.second_activity_agree);
+                boolean isAgree = agreeCB.isChecked();
+                Intent result = new Intent();
+                if (isAgree) {
+                    setResult(Activity.RESULT_OK, result);
+                } else {
+                    setResult(Activity.RESULT_CANCELED, result);
+                }
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/mad/exercise2/MainActivity.java
+++ app/src/main/java/com/mad/exercise2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mad.exercise2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -7,8 +8,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +23,23 @@ public class MainActivity extends AppCompatActivity {
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Snackbar.make(v, "Submit button clicked!", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                EditText name = findViewById(R.id.name_tf);
+                EditText email = findViewById(R.id.email_tf);
+                EditText phone = findViewById(R.id.phone_number);
+                Spinner type = findViewById(R.id.phoneSpinner);
+
+
+                Intent intent = new Intent(MainActivity.this, ActivityTwo.class);
+                intent.putExtra(ActivityTwo.NAME_EXTRA, name.getText().toString());
+                intent.putExtra(ActivityTwo.EMAIL_EXTRA, email.getText().toString());
+                intent.putExtra(ActivityTwo.TYPE_EXTRA, type.getSelectedItem().toString());
+                intent.putExtra(ActivityTwo.PHONE_EXTRA, phone.getText().toString());
+
+                startActivityForResult(intent, REQUEST_CODE);
             }
+
         });
+
 
         Button clearBtn = findViewById(R.id.main_activity_clear_all_btn);
         clearBtn.setOnClickListener(new View.OnClickListener() {
@@ -47,6 +64,22 @@ public class MainActivity extends AppCompatActivity {
                 moveTaskToBack(true);
             }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        View view = getCurrentFocus();
+        
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK ) {
+                Snackbar.make(view ,R.string.i_agree, Snackbar.LENGTH_SHORT)
+                        .setAction("Action", null).show();
+            } else {
+                Snackbar.make(view ,R.string.i_disagree, Snackbar.LENGTH_SHORT)
+                        .setAction("Action", null).show();
+            }
+        }
     }
 
 }
